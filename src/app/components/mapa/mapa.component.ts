96803c77ba8399e505a09b204d424c776a896e54
Jhon Sebastian Bohorquez Guerrero <jhonsbg@gmail.com>
2021-09-29 17:32:30 -0500

    Geolocalización de ususarios

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -32,6 +32,7 @@ export class MapaComponent implements OnInit {
       // console.log(lugares);
       this.lugares = lugares;
       this.crearMapa();
+      this.crearMarcador();
     })
 
     this.escucharSockets();
@@ -65,13 +66,26 @@ export class MapaComponent implements OnInit {
     this.mapa = new mapboxgl.Map({
       container: 'mapa',
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-75.75512993582937, 45.349977429009954],
+      center: [4.687273776841303, -74.06165854470065],
       zoom: 15.8
     });
 
     for(const [id, marcador] of Object.entries(this.lugares)){
       this.agregarMarcador(marcador);
     }
+
+    // Add geolocate control to the map.
+    this.mapa.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
   }
 
   agregarMarcador(marcador: Lugar){
@@ -98,20 +112,49 @@ export class MapaComponent implements OnInit {
       draggable: true,
       color: marcador.color
     })
-    .setLngLat([marcador.lng, marcador.lat]) 
+    // .setLngLat([marcador.lng, marcador.lat]) 
+    .setLngLat(new mapboxgl.LngLat(marcador.lng, marcador.lat)) 
     .setPopup(customPopup)
     .addTo(this.mapa);
 
-    marker.on('drag', () => {
-      const lngLat = marker.getLngLat();
-      // console.log(`Longitude: ${lngLat.lng}, Latitude: ${lngLat.lat}`);
+    navigator.geolocation.watchPosition( (position) => {
+      var latitude = position.coords.latitude; 
+      var longitude = position.coords.longitude;
+      var lngLat = new mapboxgl.LngLat(longitude, latitude);
+
+      console.log(`Longitud ${lngLat.lng} y Latitud ${lngLat.lat}`);
+
       const nuevoMarcador = {
         id: marcador.id,
-        ...lngLat
-      }
+        lng: longitude,
+        lat: latitude,
+        nombre: marcador.nombre,
+        color: marcador.color
+      } 
 
       this.wsService.emit( 'marcador-mover', nuevoMarcador );
+      this.mapa.setCenter([longitude, latitude]);
+    },
+    (err) =>{
+      console.log(err);
+    },
+    {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
     });
+
+
+    // marker.on('drag', () => {
+    //   const lngLat = marker.getLngLat();
+    //   // console.log(`Longitude: ${lngLat.lng}, Latitude: ${lngLat.lat}`);
+    //   const nuevoMarcador = {
+    //     id: marcador.id,
+    //     ...lngLat
+    //   }
+
+    //   this.wsService.emit( 'marcador-mover', nuevoMarcador );
+    // });
 
     btnBorrar.addEventListener('click', () => {
       marker.remove();
@@ -123,17 +166,41 @@ export class MapaComponent implements OnInit {
   }
 
   crearMarcador(){
-    const customMarker: Lugar = {
-      id: new Date().toISOString(),
-      lng: -75.75512993582937,
-      lat: 45.349977429009954,
-      nombre: 'Sin nombre',
-      color: '#' + Math.floor(Math.random()*16777215).toString(16) 
+    // const customMarker: Lugar = {
+    //   id: new Date().toISOString(),
+    //   lng: -57,
+    //   lat: 45.349977429009954,
+    //   nombre: 'Sin nombre',
+    //   color: '#' + Math.floor(Math.random()*16777215).toString(16) 
+    // }
+
+    // this.agregarMarcador(customMarker);
+
+    // // Emitir marcador nuevo
+    // this.wsService.emit('marcador-nuevo', customMarker);
+
+    if(!navigator.geolocation){
+      console.log('Geolocation is not supported by your browser');
+    }else{
+      navigator.geolocation.getCurrentPosition((position) => {
+        var latitude = position.coords.latitude; 
+        var longitude = position.coords.longitude;
+
+        const customMarker: Lugar = {
+          id: new Date().toISOString(),
+          lng: longitude,
+          lat: latitude,
+          nombre: 'Sin nombre',
+          color: '#' + Math.floor(Math.random()*16777215).toString(16) 
+        }
+
+        this.mapa.setCenter([longitude, latitude]);
+
+        this.agregarMarcador(customMarker);
+
+        // // Emitir marcador nuevo
+        this.wsService.emit('marcador-nuevo', customMarker);
+      })
     }
-
-    this.agregarMarcador(customMarker);
-
-    // Emitir marcador nuevo
-    this.wsService.emit('marcador-nuevo', customMarker);
   }
 }
